54a59584f9157050b397e3c3c44b8a45bcb6d79b
tejaswi0905 <73959528+tejaswi0905@users.noreply.github.com>
2026-04-06 00:49:56 +0530

    add existingShapes logic

--- apps/colab-draw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/colab-draw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { initDraw } from "@/draw";
 import { useEffect, useRef } from "react";
 
 export default function Canvas() {
@@ -6,46 +7,13 @@ export default function Canvas() {
 
   useEffect(() => {
     if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-
-      if (!ctx) {
-        return;
-      }
-
-      let clicked = false;
-      let startX = 0;
-      let startY = 0;
-
-      canvas.addEventListener("mousedown", (e) => {
-        clicked = true;
-        startX = e.clientX;
-        startY = e.clientY;
-      });
-      canvas.addEventListener("mouseup", (e) => {
-        clicked = false;
-        console.log(e.clientX);
-        console.log(e.clientY);
-      });
-      canvas.addEventListener("mousemove", (e) => {
-        if (clicked) {
-          const width = e.clientX - startX;
-          const height = e.clientY - startY;
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.strokeRect(startX, startY, width, height);
-        }
-      });
+      initDraw(canvasRef.current);
     }
   }, [canvasRef]);
 
   return (
     <div>
-      <canvas
-        ref={canvasRef}
-        width={500}
-        height={500}
-        className="bg-amber-600"
-      ></canvas>
+      <canvas ref={canvasRef} width={1080} height={1000}></canvas>
     </div>
   );
 }
