cdcc3532239b85527d4f69036b5beda931525cf1
tejaswi0905 <tejaswibiduru22@gmail.com>
2026-04-16 13:32:13 +0530

    nextjs strict type

--- apps/colab-draw-frontend/next.config.ts
+++ apps/colab-draw-frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  // @ts-expect-error - next 16+ removed this config but Vercel build might still rely on it
   eslint: {
     ignoreDuringBuilds: true,
   },
